2cf4a026ca82faa892a7dbcca301c5e0aaf9cafa
Jonathan Lopez <JonL_2086@hotmail.com>
2017-06-06 20:39:21 -0700

    Updated Script links, and animation to footer

---
+++ js/myscripts.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+"use strict";
+
+$(".bounce").on("mouseenter", function() {
+  var duration = 1;
+  TweenMax.to(this, duration / 4, {y:-50, ease:Power2.easeOut});
+  TweenMax.to(this, duration / 2, {y:0, ease:Bounce.easeOut, delay:duration / 4});
+});
